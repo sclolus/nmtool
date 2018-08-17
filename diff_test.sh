cbcbfa795c275ac19e52bfb29fd2372357e349c0
Sebastien CLOLUS <sclolus@e1r5p10.42.fr>
2018-08-17 04:19:22 +0200

    Adding my diff script

---
+++ diff_test.sh
@@ -0,0 +1,1 @@
+DIR_TARGET="/usr/bin" && make && for file in `ls -1 "$DIR_TARGET"`; do echo "$file"; ./ft_nm "$DIR_TARGET/$file" > diff.txt && nm "$DIR_TARGET/$file" > diff_bin.txt && diff diff*.txt ; done
