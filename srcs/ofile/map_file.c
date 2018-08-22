4bc0331bf3cd9c5c1d3135f2ab526d54010a3b1c
Sebastien CLOLUS <sclolus@e3r12p19.42.fr>
2018-08-22 12:00:53 +0200

    Added missing error message in case of permissions denied and such on a file

--- srcs/ofile/map_file.c
+++ srcs/ofile/map_file.c
@@ -6,7 +6,7 @@
 /*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/15 21:48:53 by sclolus           #+#    #+#             */
-/*   Updated: 2018/08/22 11:56:03 by sclolus          ###   ########.fr       */
+/*   Updated: 2018/08/22 12:00:16 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,11 @@ void	*map_file(char *filename, uint64_t *file_size)
 	*file_size = (uint64_t)stats.st_size + (4096 - (stats.st_size % 4096));
 	if (MAP_FAILED == (map = mmap(NULL, *file_size,
 						PROT_READ | PROT_WRITE, /* MAP_SHARED */ MAP_PRIVATE, fd, 0)))
-		return (NULL);// put error message
+	{
+		dprintf(2, "File does not exist or permission denied\n");
+		close(fd);
+		return (NULL);
+	}
 	close(fd); // check this ?
 	return (map);
 }
